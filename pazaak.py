a3701d0c7fa94d551384058e80b7a142ce2d12ff
Catalin Gherhes <gcatalintm@gmail.com>
2024-05-03 05:56:04 +0300

    Update help screen + misc organizing

--- pazaak.py
+++ pazaak.py
@@ -7,18 +7,14 @@ from qtd_ui import Ui_mw
 
 class GameManager:
     @classmethod
-    def start_match(cls):
-        UIManager.show_screen(2)
+    def start_deck_builder(cls):
+        UIManager.show_screen(1)
         ...
 
     @classmethod
-    def toggle_help(cls, source_page_index: int):
-        """Navigate to and from the help screen by storing previous page."""
-        if not source_page_index == 3:
-            cls.index_to_help = source_page_index
-            UIManager.show_screen(3)
-        else:
-            UIManager.show_screen(cls.index_to_help)
+    def start_match(cls):
+        UIManager.show_screen(2)
+        ...
 
 
 class UIManager:
@@ -34,6 +30,15 @@ class UIManager:
         """
         main_win.ui.sw.setCurrentIndex(index)
 
+    @classmethod
+    def toggle_help(cls, source_page_index: int):
+        """Navigate to and from the help screen by storing previous page."""
+        if not source_page_index == 3:
+            cls.index_to_help = source_page_index
+            cls.show_screen(3)
+        else:
+            cls.show_screen(cls.index_to_help)
+
 
 class Pazaak(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs) -> None:
@@ -47,17 +52,17 @@ class Pazaak(QtWidgets.QMainWindow):
 
     def setup_buttons(self):
         # Main Menu buttons.
-        self.ui.mm_btn_start.clicked.connect(lambda: UIManager.show_screen(1))
-        self.ui.mm_btn_help.clicked.connect(lambda: GameManager.toggle_help(0))
+        self.ui.mm_btn_start.clicked.connect(GameManager.start_deck_builder)
+        self.ui.mm_btn_help.clicked.connect(lambda: UIManager.toggle_help(0))
         self.ui.mm_btn_exit.clicked.connect(QtWidgets.QApplication.quit)
         # Deck Builder buttons.
         self.ui.db_btn_back.clicked.connect(lambda: UIManager.show_screen(0))
         self.ui.db_btn_play.clicked.connect(GameManager.start_match)
         # Game Screen buttons.
-        self.ui.gs_btn_help.clicked.connect(lambda: GameManager.toggle_help(2))
+        self.ui.gs_btn_help.clicked.connect(lambda: UIManager.toggle_help(2))
         self.ui.gs_btn_quit.clicked.connect(lambda: UIManager.show_screen(0))
         # Help Screen buttons.
-        self.ui.hs_btn_close.clicked.connect(lambda: GameManager.toggle_help(3))
+        self.ui.hs_btn_close.clicked.connect(lambda: UIManager.toggle_help(3))
 
 
 if __name__ == '__main__':
