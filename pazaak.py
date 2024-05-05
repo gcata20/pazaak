b56e3f7a6330ff5d777a6bd50a3768f31c936d6b
Catalin Gherhes <gcatalintm@gmail.com>
2024-05-06 01:59:25 +0300

    Develop the DeckBuilder and add features to UI.
    Write functions for initializing the deck builder,
    adding and removing cards from the deck as well as
    choosing a random one.
    Write functions for UI to set button interactivity
    and update images on buttons and labels.

--- pazaak.py
+++ pazaak.py
@@ -1,11 +1,15 @@
 from random import choice
 from sys import exit
 
-from PyQt5 import QtCore
-from PyQt5 import QtWidgets
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtWidgets as qtw
 from PyQt5.QtGui import QIcon, QPixmap
 
 from qtd_ui import Ui_mw
+
+
+MODS = ['minus', 'plus', 'dual']
+VALUES = ['1', '2', '3', '4', '5', '6']
 
 
 class Card:
@@ -27,19 +31,66 @@ class Card:
 
 class DeckManager:
     @classmethod
-    def init_deck_builder(cls):
-        cls.side_deck = [None] * 10
-        # - triggers the UI to clear deck slots and disable play button
+    def add_card(cls):
+        """Add card to the chosen cards list."""
+        new_card = mw.sender().objectName()[3:]
+        for i, deck_card in enumerate(cls.chosen_cards):
+            if not deck_card:
+                cls.chosen_cards[i] = new_card
+                obj = mw.db_card_buttons[i]
+                mod, value = new_card.split('_')
+                img_path = f'assets/card_{mod}_{value}.png'
+                UIManager.update_visual(obj, img_path, True)
+                break
+        if not cls.ready_to_play:
+            if all(cls.chosen_cards):
+                cls.ready_to_play = True
+                UIManager.set_interactive(mw.ui.db_btn_play, True)
+
+    @classmethod
+    def reset_deck_builder(cls):
+        """Reset the deck builder to initial state."""
+        UIManager.set_interactive(mw.ui.db_btn_play, False)
+        cls.ready_to_play = False
+        cls.chosen_cards = [None] * 10
+        for button in mw.db_card_buttons:
+            UIManager.update_visual(button, None, False)
     
     @classmethod
     def randomize_deck(cls):
-        ...
+        """Choose 10 random cards for the deck (max 2 copies of each)."""
+        cls.reset_deck_builder()
+        for i in range(10):
+            while True:
+                mod = choice(MODS)
+                value = choice(VALUES)
+                new_card = mod + value
+                if cls.chosen_cards.count(new_card) == 2:
+                    continue
+                break
+            cls.chosen_cards[i] = new_card
+            obj = mw.db_card_buttons[i]
+            img_path = f'assets/card_{mod}_{value}.png'
+            UIManager.update_visual(obj, img_path, True)
+        cls.ready_to_play = True
+        UIManager.set_interactive(mw.ui.db_btn_play, True)
+
+    @classmethod
+    def remove_card(cls):
+        """Remove existing card from the chosen cards list."""
+        if cls.ready_to_play:
+            cls.ready_to_play = False
+            UIManager.set_interactive(mw.ui.db_btn_play, False)
+        card_index = int(mw.sender().objectName()[-1]) - 1
+        cls.chosen_cards[card_index] = None
+        obj = mw.db_card_buttons[card_index]
+        UIManager.update_visual(obj, None, False)
 
 
 class GameManager:
     @classmethod
     def start_deck_builder(cls):
-        DeckManager.init_deck_builder()
+        DeckManager.reset_deck_builder()
         UIManager.show_screen(1)
 
     @classmethod
@@ -50,6 +101,12 @@ class GameManager:
 
 class UIManager:
     @classmethod
+    def set_interactive(cls, button: qtw.QPushButton, new_state: bool):
+        """Set a button's state of interactivity and style it accordingly."""
+        button.setEnabled(new_state)
+        # TODO: change the style sheet based on the new_state (after finishing the art)
+
+    @classmethod
     def show_screen(cls, index: int):
         """ Navigate the UI's stacked widget pages by index.
 
@@ -59,7 +116,7 @@ class UIManager:
         - 2: Game Screen
         - 3: Help Screen
         """
-        main_win.ui.sw.setCurrentIndex(index)
+        mw.ui.sw.setCurrentIndex(index)
 
     @classmethod
     def toggle_help(cls, source_page_index: int):
@@ -73,8 +130,17 @@ class UIManager:
         else:
             cls.show_screen(cls.index_to_help)
 
+    @classmethod
+    def update_visual(cls, target, img_path: str, enabled_state: bool):
+        """Update the image and state of target label or button."""
+        if isinstance(target, qtw.QLabel):
+            target.setPixmap(QPixmap(img_path))
+        elif isinstance(target, qtw.QPushButton):
+            target.setIcon(QIcon(img_path))
+        target.setEnabled(enabled_state)
 
-class Pazaak(QtWidgets.QMainWindow):
+
+class Pazaak(qtw.QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.init_ui()
@@ -88,12 +154,30 @@ class Pazaak(QtWidgets.QMainWindow):
         # Main Menu buttons.
         self.ui.mm_btn_start.clicked.connect(GameManager.start_deck_builder)
         self.ui.mm_btn_help.clicked.connect(lambda: UIManager.toggle_help(0))
-        self.ui.mm_btn_exit.clicked.connect(QtWidgets.QApplication.quit)
+        self.ui.mm_btn_exit.clicked.connect(qtw.QApplication.quit)
         # Deck Builder buttons.
         self.ui.db_btn_back.clicked.connect(lambda: UIManager.show_screen(0))
         self.ui.db_btn_play.clicked.connect(GameManager.start_match)
-        self.ui.db_btn_clear.clicked.connect(DeckManager.init_deck_builder)
+        self.ui.db_btn_clear.clicked.connect(DeckManager.reset_deck_builder)
         self.ui.db_btn_random.clicked.connect(DeckManager.randomize_deck)
+        self.av_card_buttons = [self.ui.db_plus_1, self.ui.db_plus_2,
+                                self.ui.db_plus_3, self.ui.db_plus_4,
+                                self.ui.db_plus_5, self.ui.db_plus_6,
+                                self.ui.db_minus_1, self.ui.db_minus_2,
+                                self.ui.db_minus_3, self.ui.db_minus_4,
+                                self.ui.db_minus_5, self.ui.db_minus_6,
+                                self.ui.db_dual_1, self.ui.db_dual_2,
+                                self.ui.db_dual_3, self.ui.db_dual_4,
+                                self.ui.db_dual_5, self.ui.db_dual_6]
+        for button in self.av_card_buttons:
+            button.clicked.connect(DeckManager.add_card)
+        self.db_card_buttons = [self.ui.db_deck_1, self.ui.db_deck_2,
+                                self.ui.db_deck_3, self.ui.db_deck_4,
+                                self.ui.db_deck_5, self.ui.db_deck_6,
+                                self.ui.db_deck_7, self.ui.db_deck_8,
+                                self.ui.db_deck_9, self.ui.db_deck_10]
+        for button in self.db_card_buttons:
+            button.clicked.connect(DeckManager.remove_card)
         # Game Screen buttons.
         self.ui.gs_btn_help.clicked.connect(lambda: UIManager.toggle_help(2))
         self.ui.gs_btn_quit.clicked.connect(lambda: UIManager.show_screen(0))
@@ -102,8 +186,8 @@ class Pazaak(QtWidgets.QMainWindow):
 
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication([])
-    main_win = Pazaak()
-    main_win.show()
+    app = qtw.QApplication([])
+    mw = Pazaak()
+    mw.show()
     # main_win.showFullScreen()
     exit(app.exec_())
