b0f1b67305e62fd8e767d52d206448b4d8e8d857
Catalin Gherhes <gcatalintm@gmail.com>
2024-05-10 01:22:19 +0300

    Refactor code.
    Create Competitor class from which Player and Opponent derive.
    Get rid of DeckManager, assign logic to the GameManager.
    Move Player and Opponent logic into their respective classes.
    Begin implementation of Player turn logic.

--- pazaak.py
+++ pazaak.py
@@ -35,47 +35,75 @@ class Card:
         self.img_path = f'assets/card_{new_mod}_{self.value}.png'
 
 
-class DeckManager:
-    @classmethod
-    def add_card(cls):
-        """Add card to the chosen cards list."""
-        new_card = mw.sender().objectName()[3:]
-        for i, deck_card in enumerate(cls.chosen_cards):
-            if not deck_card:
-                cls.chosen_cards[i] = new_card
-                button = mw.db_deck_cards[i]
-                mod, value = new_card.split('_')
-                img_path = f'assets/card_{mod}_{value}.png'
-                UIManager.update_visual(button, True, img_path)
-                break
-        if not cls.ready_to_play:
-            if all(cls.chosen_cards):
-                cls.ready_to_play = True
-                UIManager.update_visual(mw.ui.db_btn_play, True)
+class Competitor:
+    current_total = 0
+    side_deck = []
+    hand_cards = []
+    is_standing = False
 
     @classmethod
-    def init_deck_builder(cls):
-        """Reset the deck builder to initial state."""
-        UIManager.update_visual(mw.ui.db_btn_play, False)
-        cls.ready_to_play = False
-        cls.chosen_cards = [None] * 10
-        for button in mw.db_deck_cards:
-            UIManager.update_visual(button, False, '')
+    def generate_hand(cls):
+        """Generate a random 4-card hand from the side deck."""
+        cls.hand_cards = [None] * 4
+        for _ in range(8):
+            shuffle(cls.side_deck)
+        for i in range(4):
+            chosen_card = cls.side_deck.pop()
+            value = int(chosen_card[-1])
+            mod = chosen_card[:-2]
+            cls.hand_cards[i] = Card(value, mod)
     
     @classmethod
-    def randomize_deck(cls):
-        """Choose 10 random cards for the deck (max 2 copies of each)."""
-        cls.init_deck_builder()
+    def generate_side_deck(cls):
+        """Generate a random 10-card side deck."""
+        cls.side_deck.clear()
         for i in range(10):
             while True:
                 mod = choice(MODS)
                 value = choice(VALUES)
                 new_card = f'{mod}_{value}'
-                if cls.chosen_cards.count(new_card) == 2:
+                if cls.side_deck.count(new_card) == 2:
                     continue
                 break
-            cls.chosen_cards[i] = new_card
+            cls.side_deck.append(new_card)
+
+
+class GameManager:
+    @classmethod
+    def add_card(cls):
+        """Add chosen card to the player's side deck."""
+        chosen_card = mw.sender().objectName()[3:]
+        for i, deck_card in enumerate(Player.side_deck):
+            if not deck_card:
+                Player.side_deck[i] = chosen_card
+                button = mw.db_deck_cards[i]
+                mod, value = chosen_card.split('_')
+                img_path = f'assets/card_{mod}_{value}.png'
+                UIManager.update_visual(button, True, img_path)
+                break
+        if not cls.ready_to_play:
+            if all(Player.side_deck):
+                cls.ready_to_play = True
+                UIManager.update_visual(mw.ui.db_btn_play, True)
+
+    @classmethod
+    def init_deck_builder(cls):
+        """Set the deck builder to its initial state."""
+        UIManager.update_visual(mw.ui.db_btn_play, False)
+        cls.ready_to_play = False
+        Player.side_deck = [None] * 10
+        for button in mw.db_deck_cards:
+            UIManager.update_visual(button, False, '')
+    
+    @classmethod
+    def randomize_deck(cls):
+        """Generate a random side deck and show it on the deck builder."""
+        cls.init_deck_builder()
+        Player.generate_side_deck()
+        for i, card in enumerate(Player.side_deck):
             button = mw.db_deck_cards[i]
+            mod = card[:-2]
+            value = int(card[-1])
             img_path = f'assets/card_{mod}_{value}.png'
             UIManager.update_visual(button, True, img_path)
         cls.ready_to_play = True
@@ -83,26 +111,24 @@ class DeckManager:
 
     @classmethod
     def remove_card(cls):
-        """Remove existing card from the chosen cards list."""
+        """Remove chosen card from the player's side deck."""
         if cls.ready_to_play:
             cls.ready_to_play = False
             UIManager.update_visual(mw.ui.db_btn_play, False)
-        card_index = int(mw.sender().objectName()[-1]) - 1
-        cls.chosen_cards[card_index] = None
-        button = mw.db_deck_cards[card_index]
+        chosen_card_index = int(mw.sender().objectName()[-1]) - 1
+        Player.side_deck[chosen_card_index] = None
+        button = mw.db_deck_cards[chosen_card_index]
         UIManager.update_visual(button, False, '')
 
-
-class GameManager:
     @classmethod
     def to_deck_builder(cls):
         UIManager.show_screen(1)
-        DeckManager.init_deck_builder()
+        cls.init_deck_builder()
 
     @classmethod
     def to_game_screen(cls):
         UIManager.show_screen(2)
-        MatchManager.start_match()
+        Match.start_match()
     
     @classmethod
     def toggle_help(cls, source_page_index: int):
@@ -117,15 +143,25 @@ class GameManager:
             UIManager.show_screen(cls.index_to_help)
 
 
-class MatchManager:
+class Match:
+    house_deck = []
+
+    @classmethod
+    def generate_house_deck(cls, shuffles: int = 8) -> None:
+        """Generate a list of cards representing the house's deck.
+
+        The optional parameter 'shuffles' shuffles it that many times.
+        """
+        cls.house_deck.clear()
+        for n in range(10):
+            for _ in range(4):
+                cls.house_deck.append(n + 1)
+        for _ in range(shuffles):
+            shuffle(cls.house_deck)
+
     @classmethod
     def init_table(cls):
         """Initialize game elements for the start of a new set."""
-        # Set indicators.
-        set_indicators = mw.player_sets + mw.opp_sets
-        inactive_set_img = 'assets/set_inactive.png'
-        for label in set_indicators:
-            UIManager.update_visual(label, new_img_path=inactive_set_img)
         # Turn indicators.
         for label in [mw.player_turn, mw.ui.gs_opp_turn]:
             UIManager.update_visual(label, new_img_path='')
@@ -144,9 +180,15 @@ class MatchManager:
         """Execute logic for starting a new match."""
         # Generate player and opponent hands.
         Player.generate_hand()
+        Opponent.generate_side_deck()
         Opponent.generate_hand()
          # Disable interactible buttons.
         UIManager.toggle_gs_buttons(False)
+        # Initialize set indicators.
+        set_indicators = mw.player_sets + mw.opp_sets
+        inactive_set_img = 'assets/set_inactive.png'
+        for label in set_indicators:
+            UIManager.update_visual(label, new_img_path=inactive_set_img)
         # Show player's hand cards (and flip icons where applicable).
         for i, button in enumerate(mw.player_hand_cards):
             img_path = Player.hand_cards[i].img_path
@@ -166,56 +208,39 @@ class MatchManager:
     
     @classmethod
     def start_set(cls):
-        """Execut logic for starting a new set."""
+        """Execute logic for starting a new set."""
+        cls.generate_house_deck()
         cls.init_table()
+        Player.is_standing = False
+        Opponent.is_standing = False
+        qtc.QTimer.singleShot(500, Player.play_turn)
+
+
+class Opponent(Competitor):
+    @classmethod
+    def play_turn(cls):
         ...
 
 
-class Opponent:
-    current_total = 0
-
-    @classmethod
-    def generate_hand(cls):
-        """Generate a random 4-card hand from a random deck."""
-        side_deck = [None] * 10
-        cls.hand_cards = [None] * 4
-        for i in range(10):
-            while True:
-                mod = choice(MODS)
-                value = choice(VALUES)
-                new_card = f'{mod}_{value}'
-                if side_deck.count(new_card) == 2:
-                    continue
-                break
-            side_deck[i] = new_card
-        for _ in range(8):
-            shuffle(side_deck)
-        for i in range(4):
-            random_card = side_deck.pop()
-            value = int(random_card[-1])
-            mod = random_card[:-2]
-            new_card = Card(value, mod)
-            cls.hand_cards[i] = new_card
-    ...
-
-
-class Player:
-    current_total = 0
-    hand_cards = [None] * 4
+class Player(Competitor):
+    has_played_card = False
     
     @classmethod
-    def generate_hand(cls):
-        """Generate a random 4-card hand from the chosen side deck."""
-        cls.hand_cards = [None] * 4
-        for _ in range(8):
-            shuffle(DeckManager.chosen_cards)
-        for i in range(4):
-            chosen_card = DeckManager.chosen_cards.pop()
-            value = int(chosen_card[-1])
-            mod = chosen_card[:-2]
-            new_card = Card(value, mod)
-            cls.hand_cards[i] = new_card
-    ...
+    def play_turn(cls):
+        if not cls.is_standing:
+            cls.has_played_card = False
+            drawn_card_value = Match.house_deck.pop()
+            for label in mw.player_table_cards:
+                if not label.isEnabled():
+                    img_path = f'assets/card_basic_{drawn_card_value}.png'
+                    UIManager.update_visual(label, True, img_path)
+                    break
+            Player.current_total += drawn_card_value
+            UIManager.update_total(mw.ui.gs_player_total, drawn_card_value)
+            UIManager.toggle_gs_buttons(True)
+            # TODO: check if score == 20 or the last table slot was filled...
+        else:
+            qtc.QTimer.singleShot(100, Opponent.play_turn)
 
 
 class UIManager:
@@ -292,8 +317,8 @@ class Pazaak(qtw.QMainWindow):
         # Deck Builder buttons.
         self.ui.db_btn_back.clicked.connect(lambda: UIManager.show_screen(0))
         self.ui.db_btn_play.clicked.connect(GameManager.to_game_screen)
-        self.ui.db_btn_clear.clicked.connect(DeckManager.init_deck_builder)
-        self.ui.db_btn_random.clicked.connect(DeckManager.randomize_deck)
+        self.ui.db_btn_clear.clicked.connect(GameManager.init_deck_builder)
+        self.ui.db_btn_random.clicked.connect(GameManager.randomize_deck)
         db_av_cards = [self.ui.db_plus_1, self.ui.db_plus_2,
                        self.ui.db_plus_3, self.ui.db_plus_4,
                        self.ui.db_plus_5, self.ui.db_plus_6,
@@ -304,14 +329,14 @@ class Pazaak(qtw.QMainWindow):
                        self.ui.db_dual_3, self.ui.db_dual_4,
                        self.ui.db_dual_5, self.ui.db_dual_6]
         for button in db_av_cards:
-            button.clicked.connect(DeckManager.add_card)
+            button.clicked.connect(GameManager.add_card)
         self.db_deck_cards = [self.ui.db_deck_1, self.ui.db_deck_2,
                               self.ui.db_deck_3, self.ui.db_deck_4,
                               self.ui.db_deck_5, self.ui.db_deck_6,
                               self.ui.db_deck_7, self.ui.db_deck_8,
                               self.ui.db_deck_9, self.ui.db_deck_10]
         for button in self.db_deck_cards:
-            button.clicked.connect(DeckManager.remove_card)
+            button.clicked.connect(GameManager.remove_card)
         # Game Screen buttons and labels.
         self.ui.gs_btn_help.clicked.connect(lambda: GameManager.toggle_help(2))
         self.ui.gs_btn_quit.clicked.connect(lambda: UIManager.show_screen(0))
